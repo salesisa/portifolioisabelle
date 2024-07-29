c78ce8c77f35c34ad2e85275e5851d4791c2a7f1
salesisa <blbelaisabelle@gmail.com>
2024-07-29 18:51:23 +0000

    .

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//faz o menu aparecer apos cliclar no hamburguer
+document.querySelector('.menu-opener').addEventListener('click', ()=>{
+      let nav=document.querySelector('header nav');
+    nav.classList.toggle('opened');
+});
